0a9ffd057f6c2d632c9df235a4155de77f56cd3a
Alliine <89416288+Alliine@users.noreply.github.com>
2021-12-06 21:53:05 -0300

    Add files via upload

---
+++ components/Navegacao.vue
@@ -0,0 +1,19 @@
+<template>
+  <div >
+    <b-navbar toggleaber="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">Locauts</b-navbar-brand>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+          <b-button to="/nova-clinica" size="sm" class="mr-sm-2"
+            >Nova Clinica</b-button
+          >
+        </b-nav-form>
+        <b-nav-form>
+          <b-button to="/" size="sm" class="mr-sm-2"
+            >Todas as Clinicas</b-button
+          >
+        </b-nav-form>
+      </b-navbar-nav>
+    </b-navbar>
+  </div>
+</template>
